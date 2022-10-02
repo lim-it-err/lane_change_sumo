8f3e7c2952458188e483dc5ff2b5886522461f58
JeongHanLim <jhlim271@naver.com>
2022-10-02 19:55:41 +0900

    fixed minor bugs. TEST PASSED

--- flow_wrap/model_test.py
+++ flow_wrap/model_test.py
@@ -1,9 +1,7 @@
 from stable_baselines.common.vec_env import DummyVecEnv, SubprocVecEnv
 from stable_baselines import PPO2, SAC
 from flow.utils.rllib import get_flow_params
-
-
-env = DummyVecEnv([lambda: constructor])
+from flow.utils.registry import env_constructor
 
 class TestSingalJC():
     def __init__(self, algorithm, model_path, param_path, render=True):
@@ -13,12 +11,12 @@ class TestSingalJC():
         self.flow_params['sim'].render = render
         self.eval_env = DummyVecEnv([lambda: env_constructor(params=self.flow_params, version=0)()])
 
-    def __call__(self):
+    def test(self):
         obs = self.eval_env.reset()
         reward =0
-        for _ in range(flow_params['env'].horizon):
-            action, _states = model.predict(obs)
-            obs, rewards, dones, info = eval_env.step(action)
+        for _ in range(self.flow_params['env'].horizon):
+            action, _states = self.model.predict(obs)
+            obs, rewards, dones, info = self.eval_env.step(action)
             reward += rewards
         print('the final reward is {}'.format(reward))
 
@@ -26,7 +24,7 @@ class TestSingalJC():
         pass
 
 if __name__ == "__main__":
-    model_path = "~/baseline_results/stabilizing_open_network_merges/2022-10-02-19:20:54.zip"
-    param_path = "~/baseline_results/stabilizing_open_network_merges/2022-10-02-19:20:54.json"
+    model_path = "/home/ubuntu/baseline_results/DesiredVelocity/2022-10-02-19:38:22.zip"
+    param_path = "/home/ubuntu/baseline_results/DesiredVelocity/2022-10-02-19:38:22.json"
 
-    TestSingalJC(PPO2, model_path, param_path)
+    test = TestSingalJC(PPO2, model_path, param_path).test()
