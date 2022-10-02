112750fb6ce3fa6aa6016e374b45c83683bea909
JeongHanLim <jhlim271@naver.com>
2022-10-02 19:43:48 +0900

    Test Code for stable_baselines (Not yet tested)

--- flow_wrap/test.py
+++ flow_wrap/test.py
@@ -1,67 +1,32 @@
-import json
+from stable_baselines.common.vec_env import DummyVecEnv, SubprocVecEnv
+from stable_baselines import PPO2, SAC
+from flow.utils.rllib import get_flow_params
 
-import ray
-try:
-    from ray.rllib.agents.agent import get_agent_class
-except ImportError:
-    from ray.rllib.agents.registry import get_agent_class
-from ray.tune import run_experiments
-from ray.tune.registry import register_env
 
-from flow.utils.registry import make_create_env
-from flow.utils.rllib import FlowParamsEncoder
+env = DummyVecEnv([lambda: constructor])
 
-# number of parallel workers
-N_CPUS = 2
-# number of rollouts per training iteration
-N_ROLLOUTS = 1
+class TestSingalJC():
+    def __init__(self, algorithm, model_path, param_path, render=True):
+        #FIXME: Allow multiple algorithm
+        self.model = PPO2.load(model_path)
+        self.flow_params = get_flow_params(param_path)
+        self.flow_params['sim'].render = render
+        self.eval_env = DummyVecEnv([lambda: env_constructor(params=self.flow_params, version=0)()])
 
-ray.init(num_cpus=N_CPUS)
+    def __call__(self):
+        obs = self.eval_env.reset()
+        reward =0
+        for _ in range(flow_params['env'].horizon):
+            action, _states = model.predict(obs)
+            obs, rewards, dones, info = eval_env.step(action)
+            reward += rewards
+        print('the final reward is {}'.format(reward))
 
-# The algorithm or model to train. This may refer to "
-#      "the name of a built-on algorithm (e.g. RLLib's DQN "
-#      "or PPO), or a user-defined trainable function or "
-#      "class registered in the tune registry.")
-alg_run = "PPO"
+    def __del__(self):
+        pass
 
-agent_cls = get_agent_class(alg_run)
-config = agent_cls._default_config.copy()
-config["num_workers"] = N_CPUS - 1  # number of parallel workers
-config["train_batch_size"] = HORIZON * N_ROLLOUTS  # batch size
-config["gamma"] = 0.999  # discount rate
-config["model"].update({"fcnet_hiddens": [16, 16]})  # size of hidden layers in network
-config["use_gae"] = True  # using generalized advantage estimation
-config["lambda"] = 0.97  
-config["sgd_minibatch_size"] = min(16 * 1024, config["train_batch_size"])  # stochastic gradient descent
-config["kl_target"] = 0.02  # target KL divergence
-config["num_sgd_iter"] = 10  # number of SGD iterations
-config["horizon"] = HORIZON  # rollout horizon
+if __name__ == "__main__":
+    model_path = "~/baseline_results/stabilizing_open_network_merges/2022-10-02-19:20:54.zip"
+    param_path = "~/baseline_results/stabilizing_open_network_merges/2022-10-02-19:20:54.json"
 
-# save the flow params for replay
-flow_json = json.dumps(flow_params, cls=FlowParamsEncoder, sort_keys=True,
-                       indent=4)  # generating a string version of flow_params
-config['env_config']['flow_params'] = flow_json  # adding the flow_params to config dict
-config['env_config']['run'] = alg_run
-
-# Call the utility function make_create_env to be able to 
-# register the Flow env for this experiment
-create_env, gym_name = make_create_env(params=flow_params, version=0)
-
-# Register as rllib env with Gym
-register_env(gym_name, create_env)
-
-trials = run_experiments({
-    flow_params["exp_tag"]: {
-        "run": alg_run,
-        "env": gym_name,
-        "config": {
-            **config
-        },
-        "checkpoint_freq": 1,  # number of iterations between checkpoints
-        "checkpoint_at_end": True,  # generate a checkpoint at the end
-        "max_failures": 999,
-        "stop": {  # stopping conditions
-            "training_iteration": 1,  # number of iterations to stop after
-        },
-    },
-})
+    TestSingalJC(PPO2, model_path, param_path)
